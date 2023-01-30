8caa64723d14001290b7f1ab47546de43e14f570
thisisjab <heythisisjab@gmail.com>
2023-01-30 12:57:02 +0330

    Add login and logout redirection URLs

--- custom_user_model_project/settings.py
+++ custom_user_model_project/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Login and logout redirection URLs
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
