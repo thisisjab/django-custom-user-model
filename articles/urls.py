8f7aa1be264c16d445b980c9c815be2f66269596
thisisjab <heythisisjab@gmail.com>
2023-02-14 22:42:36 +0330

    Reformat file

--- articles/urls.py
+++ articles/urls.py
@@ -9,11 +9,11 @@ from .views import (
 
 urlpatterns = [
     path("<int:pk>/", ArticleDetailView.as_view(),
-        name="article_detail"),
+         name="article_detail"),
     path("<int:pk>/edit/", ArticleUpdateView.as_view(),
-        name="article_edit"),
+         name="article_edit"),
     path("<int:pk>/delete/", ArticleDeleteView.as_view(),
-        name="article_delete"),
+         name="article_delete"),
     path("", ArticleListView.as_view(),
-        name="article_list"),
+         name="article_list"),
 ]
